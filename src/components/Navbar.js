6602ea215ebdb6947c14c463c9f77291931e5587
zmahjoub <zmahjoub99@gmail.com>
2023-06-11 17:52:33 +0330

    feat: added header

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import Image1 from "../images/1.jpg";
+
+const Navbar = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.listContainer}>
+        <ul className={styles.list}>
+          <li>Home Page</li>
+          <li>Products</li>
+          <li>About Us</li>
+        </ul>
+      </div>
+      <div className={styles.logo}>
+        <img className={styles.logo} src={Image1} alt="Logo" />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
