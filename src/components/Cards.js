f8a9a3491227e3d96aaf6263a25fd783cfe561d5
zmahjoub <zmahjoub99@gmail.com>
2023-06-14 15:24:15 +0330

    ref: used list rendering

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,13 +8,24 @@ import Image7 from "../images/7.jpeg";
 import Image8 from "../images/8.jpg";
 
 export default class Cards extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        { image: Image1, name: "Fairytale Tree Cottage", cost: "11000 US$" },
+        { image: Image7, name: "House of Love", cost: "12000 US$" },
+        { image: Image3, name: "Sweet Cottage by Lake", cost: "9690 US$" },
+        { image: Image8, name: "Modern Hill Mansion", cost: "124000 US$" },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className={styles.container}>
-        <Card image={Image1} name="Fairytale Tree Cottage" cost="11000 US$" />
-        <Card image={Image7} name="House of Love" cost="12000 US$" />
-        <Card image={Image3} name="Sweet Cottage by Lake" cost="9690 US$" />
-        <Card image={Image8} name="Modern Hill Mansion" cost="124000 US$" />
+        {this.state.data.map((item) => (
+          <Card image={item.image} name={item.name} cost={item.cost} />
+        ))}
       </div>
     );
   }
