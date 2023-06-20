62618d9c1b636fd7eca9d4d182b25589316fe2d5
zmahjoub <zmahjoub99@gmail.com>
2023-06-20 19:03:05 +0330

    ref: made Landing component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import Cards from "./components/Cards";
-import Search from "./components/Search";
-import Logos from "./components/Logos";
+
 import Footer from "./components/Footer";
+import Landing from "./components/Landing";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Banner />
-        <Cards />
-        <Search />
-        <Logos />
+        <Landing />
         <Footer />
       </div>
     );
