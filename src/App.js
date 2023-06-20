efd66850e8c547935910a59138aba70a27b0555f
zmahjoub <zmahjoub99@gmail.com>
2023-06-20 19:26:53 +0330

    fix: changed the version of react router dom and used switch for route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Navbar from "./components/Navbar";
 
 import Footer from "./components/Footer";
 import Landing from "./components/Landing";
+import { Route, Switch } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Landing />
+        <Switch>
+          <Route to="/" component={Landing} />
+        </Switch>
         <Footer />
       </div>
     );
